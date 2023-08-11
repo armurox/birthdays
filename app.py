48a57251259ea724303b8ff3eba79fb8e41119da
Arman Jasuja <arman_jasuja@yahoo.com>
2023-08-11 21:34:53 +0530

    Login function implemented

--- app.py
+++ app.py
@@ -57,5 +57,37 @@ def index():
 
 @app.route("/login", methods = ["GET", "POST"])
 def login():
-    
+    """Log user in"""
 
+    # Forget any user_id
+    session.clear()
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return apology("must provide username", 403)
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return apology("must provide password", 403)
+
+        # Query database for username
+        rows = db.execute("SELECT * FROM users WHERE username = (?)", (request.form.get("username"),))
+
+        rows = [dict(i) for i in rows]
+        # Ensure username exists and password is correct
+        if len(rows) != 1 or not check_password_hash(
+            rows[0]["hash"], request.form.get("password")
+        ):
+            return apology("invalid username and/or password", 403)
+
+        # Remember which user has logged in
+        session["user_id"] = rows[0]["id"]
+
+        # Redirect user to home page
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("login.html")
