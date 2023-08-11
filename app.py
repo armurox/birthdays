34535a4bff4411031804e5b677112a853e55f237
Arman Jasuja <arman_jasuja@yahoo.com>
2023-08-11 21:34:09 +0530

    Non-cache responses added and helpers library added

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 import sqlite3
 from flask import Flask, flash, jsonify, redirect, render_template, request, session
+from flask_session import Session
+from helpers import login_required, apology
+
 
 # Configure application
 app = Flask(__name__)
@@ -7,15 +10,28 @@ app = Flask(__name__)
 # Ensure templates are auto-reloaded
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
-# Configure CS50 Library to use SQLite database
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Configure SQLite database
 con = sqlite3.connect("birthdays.db", check_same_thread=False)
 con.row_factory = sqlite3.Row
 db = con.cursor()
 
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
 @app.route("/", methods=["GET", "POST"])
+@login_required
 def index():
     if request.method == "POST":
-
         # Add the user's entry into the database
         name = request.form.get("name")
         while True:
@@ -39,4 +55,7 @@ def index():
         birthdays = db.execute("SELECT * FROM birthdays")
         return render_template("index.html", birthdays = birthdays)
 
+@app.route("/login", methods = ["GET", "POST"])
+def login():
+    
 
